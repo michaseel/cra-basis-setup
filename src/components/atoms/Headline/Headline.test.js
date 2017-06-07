2ce2d67ebd513391c0faa16e07c2a94c7252b945
Michael Seel <michael.seel@nexum.de>
2017-06-07 16:13:49 +0200

    lint-staged & prettier

--- src/components/atoms/Headline/Headline.test.js
+++ src/components/atoms/Headline/Headline.test.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Headline from './Headline';
+import React from "react";
+import renderer from "react-test-renderer";
+import Headline from "./Headline";
 
-it('renders Headline snapshot', () => {
-    const tree = renderer.create(
-        <Headline>Test</Headline>
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+it("renders Headline snapshot", () => {
+  const tree = renderer.create(<Headline>Test</Headline>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,27 +1,25 @@
-
-
 module.exports = {
-    title: 'CRA Styleguide',
-    sections: [
-      {
-        name: 'Atoms',
-        content: 'src/components/atoms/atoms.md',
-        components: 'src/components/atoms/**/[A-Z]*.js',
-      },
-      {
-        name: 'Molecules',
-        content: 'src/components/molecules/molecules.md',
-        components: 'src/components/molecules/**/[A-Z]*.js',
-      },
-      {
-        name: 'Organisms',
-        content: 'src/components/organisms/organisms.md',
-        components: 'src/components/organisms/**/[A-Z]*.js',
-      },
-      {
-        name: 'Templates',
-        content: 'src/components/templates/templates.md',
-        components: 'src/components/templates/**/[A-Z]*.js',
-      },
-    ],
+  title: "CRA Styleguide",
+  sections: [
+    {
+      name: "Atoms",
+      content: "src/components/atoms/atoms.md",
+      components: "src/components/atoms/**/[A-Z]*.js"
+    },
+    {
+      name: "Molecules",
+      content: "src/components/molecules/molecules.md",
+      components: "src/components/molecules/**/[A-Z]*.js"
+    },
+    {
+      name: "Organisms",
+      content: "src/components/organisms/organisms.md",
+      components: "src/components/organisms/**/[A-Z]*.js"
+    },
+    {
+      name: "Templates",
+      content: "src/components/templates/templates.md",
+      components: "src/components/templates/**/[A-Z]*.js"
+    }
+  ]
 };
