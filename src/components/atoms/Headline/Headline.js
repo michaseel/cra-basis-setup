9e387baa270f2c549838feaad085a94800e5ac7c
Michael Seel <michael.seel@nexum.de>
2017-06-07 16:14:38 +0200

    test prettier

--- src/components/atoms/Headline/Headline.js
+++ src/components/atoms/Headline/Headline.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const H1 = styled.h1`
   font-size: 1.5em;
@@ -7,6 +7,10 @@ const H1 = styled.h1`
   color: palevioletred;
 `;
 
-const Headline = ({ children }) => (<H1> { children }</H1>);
+const Headline = ({ children }) =>
+  <H1>
+    {" "}{children}
+
+  </H1>;
 
 export default Headline;
