13ef13119b51aeb0e2e6773e7ccf5f6f3154a686
Michael Seel <michael.seel@nexum.de>
2017-06-23 09:23:02 +0200

    add jest-styled-components for better snapshot testing!

--- src/components/atoms/Headline/Headline.test.js
+++ src/components/atoms/Headline/Headline.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
 import Headline from './Headline';
 
 it('renders Headline snapshot', () => {
   const tree = renderer.create(<Headline>Test</Headline>).toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(tree).toMatchStyledComponentsSnapshot();
 });
